5e5a0f82435ce2b7f67dc06002f60d3ba25fda45
Roman <roman@lerchster.dev>
2024-09-30 00:29:11 +0200

    2024-09-30 00:29:11

--- system/configuration.nix
+++ system/configuration.nix
@@ -39,5 +39,13 @@
     };
   };
 
+  nix.optimise.automatic = true;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
 }
