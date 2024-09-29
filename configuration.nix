5b2fa3f3e003254d4906dc65435a044e7e131abf
Roman <roman@lerchster.dev>
2024-09-29 21:44:07 +0200

    2024-09-29 21:44:07

--- configuration.nix
+++ configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      # ./zsh/configuration.nix
+      ./zsh/configuration.nix
     ];
 
   # Bootloader.
