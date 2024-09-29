0b2549f4b888e37f198b6f6474773c02181b0781
Roman <roman@lerchster.dev>
2024-09-29 21:44:38 +0200

    2024-09-29 21:44:38

--- configuration.nix
+++ configuration.nix
@@ -144,24 +144,6 @@
   # Install firefox.
   programs.firefox.enable = true;
 
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      nmn = "nug && ngb";
-      nug = "yes | protonup; sudo nix flake update ~/.nixos; nup";
-      nup = "sudo nixos-rebuild switch --flake ~/.nixos#default";
-      ngb = "git -C ~/.nixos add . && git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" && git -C ~/.nixos push";
-    };
-    ohMyZsh = {
-      enable = true;
-      plugins = ["git"];
-      theme = "robbyrussell";
-    };
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
--- zsh/configuration.nix
+++ zsh/configuration.nix
@@ -5,4 +5,21 @@
 { config, pkgs, inputs, ... }:
 
 {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      nmn = "nug && ngb";
+      nug = "yes | protonup; sudo nix flake update ~/.nixos; nup";
+      nup = "sudo nixos-rebuild switch --flake ~/.nixos#default";
+      ngb = "git -C ~/.nixos add . && git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" && git -C ~/.nixos push";
+    };
+    ohMyZsh = {
+      enable = true;
+      plugins = ["git"];
+      theme = "robbyrussell";
+    };
+  };
 }
