724151d4e176a05d1d97551322ec3480360a8072
Roman <roman@lerchster.dev>
2024-10-02 22:19:17 +0200

    2024-10-02 22:19:17

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very based flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
