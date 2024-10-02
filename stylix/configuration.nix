1655b7d8209c45837f00d639fa0f8a63233fbe5c
Roman <roman@lerchster.dev>
2024-10-02 23:51:12 +0200

    2024-10-02 23:51:12

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -5,7 +5,29 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.polarity = "dark";
   stylix.image = ./wallpaper.jpg;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+
+  stylix.fonts = {
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serif";
+    };
+
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+
+    monospace = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans Mono";
+    };
+
+    emoji = {
+      package = pkgs.noto-fonts-emoji;
+      name = "Noto Color Emoji";
+    };
+  };
 }
