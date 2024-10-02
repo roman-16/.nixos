247576a545723798500e88ce5767a6eafbe3ed37
Roman <roman@lerchster.dev>
2024-10-02 23:49:02 +0200

    2024-10-02 23:49:02

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   # stylix.polarity = "dark";
   stylix.image = ./wallpaper.jpg;
 }
