57f57289118aaf4bd91e9a1a4aee7fe7799677f4
Roman <roman@lerchster.dev>
2024-10-02 23:46:33 +0200

    2024-10-02 23:46:33

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -7,8 +7,5 @@
   stylix.enable = true;
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
   stylix.polarity = "dark";
-  stylix.image = pkgs.fetchurl {
-    url = "https://images.pexels.com/photos/19394170/pexels-photo-19394170.jpeg?cs=srgb&dl=pexels-njeromin-19394170.jpg&fm=jpg&w=5970&h=3342";
-    hash = "sha256-w2GzzhokUBH5XRWeczHeMydR5ivwt9fCO6hxZAcNypU=";
-  };
+  stylix.image = ./wallpaper.jpg;
 }
