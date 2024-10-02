82102a218ebc5d6f28ba02be9a40df2aaf863f26
Roman <roman@lerchster.dev>
2024-10-02 23:49:49 +0200

    2024-10-02 23:49:49

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -6,6 +6,6 @@
 }: {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  # stylix.polarity = "dark";
+  stylix.polarity = "dark";
   stylix.image = ./wallpaper.jpg;
 }
