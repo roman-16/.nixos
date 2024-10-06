596eed63da4c95c37f152fc6f11ad8e743112eef
Roman <roman@lerchster.dev>
2024-10-06 18:59:41 +0200

    2024-10-06 18:59:41

--- firefox/home.nix
+++ firefox/home.nix
@@ -8,6 +8,14 @@
     enable = true;
     profiles.default = {
       isDefault = true;
+
+      search = {
+        force = true;
+        order = ["DuckDuckGo"];
+        default = "DuckDuckGo";
+        privateDefault = "DuckDuckGo";
+      };
+
       userChrome = ''
         #alltabs-button {
           display: none !important;
