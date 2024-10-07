c0af2f8a94e476b2465b4acb1c0fa6414b0b6cf0
Roman <roman@lerchster.dev>
2024-10-07 22:53:48 +0200

    2024-10-07 22:53:48

--- programs/configuration.nix
+++ programs/configuration.nix
@@ -19,11 +19,6 @@
     just
     nerdfonts
     clapper
-    gst_all_1.gst-plugins-base
-    gst_all_1.gstreamer
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
   ];
 
   services.xserver.excludePackages = with pkgs; [
