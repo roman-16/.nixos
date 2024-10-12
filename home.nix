d685ff5409f16f1f3c1b82a5e503d6777e07ad2e
Roman <roman@lerchster.dev>
2024-10-12 23:49:32 +0200

    2024-10-12 23:49:32

--- home.nix
+++ home.nix
@@ -4,7 +4,6 @@
   ...
 }: {
   imports = [
-    ./direnv/home.nix
     ./firefox/home.nix
     ./git/home.nix
     ./gnome/home.nix
--- zsh/home.nix
+++ zsh/home.nix
@@ -3,40 +3,48 @@
   pkgs,
   ...
 }: {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    shellAliases = {
-      nmn = "nft && ngs && nug && ngs && ngb";
-      nug = "yes | protonup; sudo nix flake update ~/.nixos; nup";
-      nup = "sudo nixos-rebuild switch --flake ~/.nixos#default";
-      ngs = "git -C ~/.nixos add .";
-      ngb = "git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" && git -C ~/.nixos push";
-      nft = "alejandra -q ~/.nixos";
-      nde = "alejandra -q *; nix develop -c \"$SHELL -c \"code .\"\"";
-
-      ls = "eza --icons=always --color=always --group-directories-first --hyperlink";
-      la = "ls -a";
-
-      cat = "bat -p";
-
-      grr = "git branch | grep -v \\* | xargs git branch -D; git remote prune origin";
-
-      fastfetch = "fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc";
-    };
-    initExtra = ''
-      source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh;
-
-      fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc;
-    '';
-    oh-my-zsh = {
+  programs = {
+    direnv = {
       enable = true;
-      plugins = ["direnv" "git"];
-      theme = "robbyrussell";
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    fzf.enable = true;
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      shellAliases = {
+        nmn = "nft && ngs && nug && ngs && ngb";
+        nug = "yes | protonup; sudo nix flake update ~/.nixos; nup";
+        nup = "sudo nixos-rebuild switch --flake ~/.nixos#default";
+        ngs = "git -C ~/.nixos add .";
+        ngb = "git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\" && git -C ~/.nixos push";
+        nft = "alejandra -q ~/.nixos";
+        nde = "alejandra -q *; nix develop -c \"$SHELL -c \"code .\"\"";
+
+        ls = "eza --icons=always --color=always --group-directories-first --hyperlink";
+        la = "ls -a";
+
+        cat = "bat -p";
+
+        grr = "git branch | grep -v \\* | xargs git branch -D; git remote prune origin";
+
+        fastfetch = "fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc";
+      };
+      initExtra = ''
+        source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh;
+
+        fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc;
+      '';
+      oh-my-zsh = {
+        enable = true;
+        plugins = ["direnv" "git"];
+        theme = "robbyrussell";
+      };
     };
   };
-
-  programs.fzf.enable = true;
 }
