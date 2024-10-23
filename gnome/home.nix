62ba75a7ad84af5563dcbb7d5839a9bbbff18697
Roman <roman@lerchster.dev>
2024-10-23 18:58:51 +0200

    2024-10-23 18:58:51

--- gnome/home.nix
+++ gnome/home.nix
@@ -94,7 +94,7 @@
     "org/gnome/settings-daemon/plugins/color" = {
       night-light-enabled = true;
       night-light-schedule-automatic = true;
-      night-light-temperature = lib.hm.gvariant.mkUint32 2700;
+      night-light-temperature = lib.hm.gvariant.mkUint32 3000;
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
