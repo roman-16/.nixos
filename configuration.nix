4e309a29257119f76a9d524016e8efa33136952b
Roman <roman@lerchster.dev>
2024-12-03 19:54:24 +0100

    2024-12-03 19:54:24

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,6 @@
 
     ./drivers/configuration.nix
     ./firefox/configuration.nix
-    ./gdlauncher/configuration.nix
     ./git/configuration.nix
     ./gnome/configuration.nix
     ./locale/configuration.nix
