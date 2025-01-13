21884abf2e7a39bb6a6df296b104eafa86f8206f
Roman <roman@lerchster.dev>
2025-01-13 19:57:58 +0100

    2025-01-13 19:57:58

--- fonts/configuration.nix
+++ fonts/configuration.nix
@@ -1,6 +1,17 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    nerd-fonts.sauce-code-pro
-    fira-code
-  ];
+  fonts = {
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      nerd-fonts.sauce-code-pro
+      fira-code
+    ];
+
+    fontconfig = {
+      defaultFonts = {
+        serif = ["Cantarell"];
+        sansSerif = ["Cantarell"];
+        monospace = ["Fira Code"];
+      };
+    };
+  };
 }
