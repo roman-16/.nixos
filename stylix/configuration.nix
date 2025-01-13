4da793b39c4cb609978cb59696b5f31d3f1cd54d
Roman <roman@lerchster.dev>
2025-01-13 20:08:39 +0100

    2025-01-13 20:08:39

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -1,41 +1,43 @@
 {pkgs, ...}: {
-  stylix.enable = true;
-  stylix.polarity = "dark";
-  stylix.image = ./wallpaper.jpg;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-black.yaml";
+  stylix = {
+    enable = true;
+    polarity = "dark";
+    image = ./wallpaper.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-black.yaml";
 
-  stylix.cursor = {
-    package = pkgs.vimix-cursors;
-    name = "Vimix-white-cursors";
-    size = 32;
-  };
-
-  stylix.fonts = {
-    serif = {
-      package = pkgs.cantarell-fonts;
-      name = "Cantarell";
+    cursor = {
+      package = pkgs.vimix-cursors;
+      name = "Vimix-white-cursors";
+      size = 32;
     };
 
-    sansSerif = {
-      package = pkgs.cantarell-fonts;
-      name = "Cantarell";
-    };
+    fonts = {
+      serif = {
+        package = pkgs.cantarell-fonts;
+        name = "Cantarell";
+      };
 
-    monospace = {
-      package = pkgs.fira-code;
-      name = "Fira Code";
-    };
+      sansSerif = {
+        package = pkgs.cantarell-fonts;
+        name = "Cantarell";
+      };
 
-    emoji = {
-      package = pkgs.noto-fonts-emoji;
-      name = "Noto Color Emoji";
-    };
+      monospace = {
+        package = pkgs.fira-code;
+        name = "Fira Code";
+      };
 
-    sizes = {
-      applications = 11;
-      desktop = 9;
-      popups = 9;
-      terminal = 11;
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+
+      sizes = {
+        applications = 11;
+        desktop = 9;
+        popups = 9;
+        terminal = 11;
+      };
     };
   };
 }
