0f9b11067f86e275d67768284632ef08dc38f74e
Roman <roman@lerchster.dev>
2025-03-02 15:28:26 +0100

    2025-03-02 15:28:26

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -3,7 +3,7 @@
     enable = true;
     polarity = "dark";
     image = ./wallpaper.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/pandora.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/eris.yaml";
 
     cursor = {
       package = pkgs.vimix-cursors;
