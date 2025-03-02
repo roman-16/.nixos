9f62cd593e5f48e8b86d564c7f366648c8e26606
Roman <roman@lerchster.dev>
2025-03-02 15:31:08 +0100

    2025-03-02 15:31:08

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -3,7 +3,7 @@
     enable = true;
     polarity = "dark";
     image = ./wallpaper.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/horizon-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-black.yaml";
 
     cursor = {
       package = pkgs.vimix-cursors;
