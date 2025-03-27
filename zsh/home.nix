cfa254cee69d2ba04d73ef6a45867559f2b129d9
Roman <roman@lerchster.dev>
2025-03-27 19:54:56 +0100

    2025-03-27 19:54:56

--- zsh/home.nix
+++ zsh/home.nix
@@ -30,24 +30,26 @@
       autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
       shellAliases = {
+        cat = "bat -p";
+
+        cd = "z";
+
+        fastfetch = "fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc";
+
+        grep = "rg";
+
+        grr = "git branch | grep -v \\* | xargs git branch -D; git remote prune origin";
+
+        ls = "eza --icons=always --color=always --group-directories-first --hyperlink";
+        ll = "ls -lh";
+        la = "ls -lha";
+
         nmn = "nft && ngp && ngs && nup && ngs && ngb";
         nup = "nh os switch -uH default";
         ngp = "git -C ~/.nixos pull";
         ngs = "git -C ~/.nixos add .";
         ngb = "git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\"; git -C ~/.nixos push";
         nft = "alejandra -q ~/.nixos";
-
-        ls = "eza --icons=always --color=always --group-directories-first --hyperlink";
-        ll = "ls -lh";
-        la = "ls -lha";
-
-        cat = "bat -p";
-
-        grep = "rg";
-
-        grr = "git branch | grep -v \\* | xargs git branch -D; git remote prune origin";
-
-        fastfetch = "fastfetch -l small -c ~/.nixos/zsh/fastfetch.jsonc";
       };
       initExtra = ''
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh;
