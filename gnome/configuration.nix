51a65415e54b40094ab57d46e367113a79705077
Roman <roman@lerchster.dev>
2025-06-01 15:59:53 +0200

    2025-06-01 15:59:53

--- gnome/configuration.nix
+++ gnome/configuration.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   environment.systemPackages = with pkgs; [
     gnome-tweaks
