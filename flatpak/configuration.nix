504459bbcbc92217a5060be318aae1ccef0e310b
Roman <roman@lerchster.dev>
2025-06-06 21:55:34 +0200

    2025-06-06 21:55:34

--- flatpak/configuration.nix
+++ flatpak/configuration.nix
@@ -1,8 +1,18 @@
-{...}: {
+{pkgs, ...}: {
   services.flatpak = {
     enable = true;
     packages = [
       "io.github.Soundux"
+    ];
+  };
+
+  xdg.portal = {
+    enable = true;
+    config.common.default = ["gtk"];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+      kdePackages.xdg-desktop-portal-kde
     ];
   };
 }
