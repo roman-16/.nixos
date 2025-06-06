b3ac60de66f3be7bf67f6a8fc5352087d9958c64
Roman <roman@lerchster.dev>
2025-06-06 21:44:20 +0200

    2025-06-06 21:44:20

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,7 @@
 
     ./drivers/configuration.nix
     ./firefox/configuration.nix
+    ./flatpak/configuration.nix
     ./fonts/configuration.nix
     ./git/configuration.nix
     ./gnome/configuration.nix
