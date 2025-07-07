19d8d9dc5e19871ffef598b1f19b0991efd12d72
Roman <roman@lerchster.dev>
2025-07-08 00:01:22 +0200

    2025-07-08 00:01:22

--- stylix/configuration.nix
+++ stylix/configuration.nix
@@ -3,7 +3,7 @@
     enable = true;
     polarity = "dark";
     image = ./wallpaper.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
 
     cursor = {
       package = pkgs.nordzy-cursor-theme;
