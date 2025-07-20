6fe40a110a73d185299053785c1b31b59fd45b72
Roman <roman@lerchster.dev>
2025-07-20 02:24:08 +0200

    2025-07-20 02:24:08

--- zsh/home.nix
+++ zsh/home.nix
@@ -60,7 +60,7 @@
         ngb = "git -C ~/.nixos commit -m \"$(date '+%Y-%m-%d %H:%M:%S')\"; git -C ~/.nixos push";
         nft = "alejandra -q ~/.nixos";
 
-        tree = "tre";
+        tree = "tre --editor";
       };
       initContent = ''
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh;
