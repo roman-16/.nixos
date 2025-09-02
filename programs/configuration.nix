389894d0c57036951659f365c6cb844c5f6d31f0
Roman <roman@lerchster.dev>
2025-09-02 22:44:46 +0200

    2025-09-02 22:44:46

--- programs/configuration.nix
+++ programs/configuration.nix
@@ -1,4 +1,12 @@
 {pkgs, ...}: {
+  xdg.mime.defaultApplications = {
+    "text/html" = "brave-browser.desktop";
+    "x-scheme-handler/http" = "brave-browser.desktop";
+    "x-scheme-handler/https" = "brave-browser.desktop";
+    "x-scheme-handler/about" = "brave-browser.desktop";
+    "x-scheme-handler/unknown" = "brave-browser.desktop";
+  };
+
   environment.systemPackages = with pkgs; [
     micro
     vscode.fhs
