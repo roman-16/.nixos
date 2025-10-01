0b672b5ae53128315c96cc8d8a509f7b28549d75
Roman <roman@lerchster.dev>
2025-10-01 19:06:46 +0200

    2025-10-01 19:06:46

--- opencode/home.nix
+++ opencode/home.nix
@@ -3,6 +3,16 @@
     enable = true;
     settings = {
       autoupdate = false;
+      mcp = {
+        context7 = {
+          enabled = true;
+          headers = {
+            CONTEXT7_API_KEY = "YOUR_API_KEY";
+          };
+          type = "remote";
+          url = "https://mcp.context7.com/mcp";
+        };
+      };
       model = "google/gemini-2.5-pro";
       permission = {
         edit = "allow";
