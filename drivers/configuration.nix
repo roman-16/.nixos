662ea22315188bcc5bdbf8dccf50b445b3d9257b
Roman <roman@lerchster.dev>
2025-10-03 20:56:54 +0200

    2025-10-03 20:56:54

--- drivers/configuration.nix
+++ drivers/configuration.nix
@@ -1,16 +1,21 @@
 {config, ...}: {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    opengl.enable = true;
   };
 
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
 }
