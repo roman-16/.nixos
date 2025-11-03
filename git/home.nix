3affb0c73ca4ef1a820b07126f43b3b6f1238d8d
Roman <roman@lerchster.dev>
2025-11-03 19:01:46 +0100

    2025-11-03 19:01:46

--- git/home.nix
+++ git/home.nix
@@ -1,10 +1,6 @@
 {...}: {
   programs.git = {
     enable = true;
-    # userEmail = "roman@lerchster.dev";
-    # userName = "Roman";
-    # extraConfig = {
-    # };
 
     settings = {
       init.defaultBranch = "main";
