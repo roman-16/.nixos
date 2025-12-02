64319fb0d67d5aa383b735be28c8e8f8a816b504
Roman <roman@lerchster.dev>
2025-12-02 15:26:20 +0100

    2025-12-02 15:26:20

--- programs/configuration.nix
+++ programs/configuration.nix
@@ -23,6 +23,15 @@
     gimp3-with-plugins
     gparted
     file-roller
+
+    # GStreamer plugins for video/audio support
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
   ];
 
   services.xserver.excludePackages = with pkgs; [
