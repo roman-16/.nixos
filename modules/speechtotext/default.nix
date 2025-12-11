2e9edfa7858cfa4f8f37fa59be2c63b44f56f4d5
Roman <roman@lerchster.dev>
2025-12-11 16:32:04 +0100

    2025-12-11 16:32:04

--- modules/speechtotext/default.nix
+++ modules/speechtotext/default.nix
@@ -1,65 +1,52 @@
 {
-  nixos = {pkgs, ...}: let
-    whisper-cuda = pkgs.whisper-cpp.override {
-      cudaSupport = true;
-    };
-  in {
+  nixos = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
-      alsa-utils # provides arecord
-      whisper-cuda
+      alsa-utils
+      (whisper-cpp.override {
+        cudaSupport = true;
+      })
       wl-clipboard
-      dotool # keyboard simulation with layout support
+      dotool
     ];
   };
 
   home = {pkgs, ...}: let
-    whisper-cuda = pkgs.whisper-cpp.override {
-      cudaSupport = true;
-    };
-
-    whisperModel = pkgs.fetchurl {
+    extensionDirectory = ".local/share/gnome-shell/extensions/speechtotext@local";
+    model = pkgs.fetchurl {
       url = "https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.bin";
       hash = "sha256-YO1bw90U7qhWST0zQ0m0BXgt3K8AKNS130CINF+6Lv4=";
     };
-
-    recordScript = pkgs.runCommand "stt-record.sh" {} ''
-      ${pkgs.gnused}/bin/sed \
-        -e 's|@model@|${whisperModel}|g' \
-        -e 's|@whisper@|${whisper-cuda}|g' \
-        -e 's|@dotool@|${pkgs.dotool}|g' \
-        ${./record.sh} > $out
-      chmod +x $out
-    '';
-
-    # Compile GSettings schema
-    compiledSchemas =
-      pkgs.runCommand "speechtotext-schemas" {
+    schemas =
+      pkgs.runCommand "stt-schemas" {
         nativeBuildInputs = [pkgs.glib];
       } ''
-        mkdir -p $out
-        cp ${./schemas/org.gnome.shell.extensions.speechtotext.gschema.xml} $out/
-        glib-compile-schemas $out
+        mkdir -p $out && cp ${./schemas/org.gnome.shell.extensions.speechtotext.gschema.xml} $out/ && glib-compile-schemas $out
       '';
-
-    extDir = ".local/share/gnome-shell/extensions/speechtotext@local";
-  in {
-    home.file = {
-      ".local/bin/stt-record.sh" = {
-        source = recordScript;
-        executable = true;
-      };
-
-      "${extDir}/extension.js".source = ./extension.js;
-      "${extDir}/metadata.json".source = ./metadata.json;
-      "${extDir}/stylesheet.css".source = ./stylesheet.css;
-      "${extDir}/schemas" = {
-        source = compiledSchemas;
-        recursive = true;
-      };
+    script = pkgs.runCommand "stt-record.sh" {} ''
+      sed -e 's|@model@|${model}|' -e 's|@whisper@|${whisper}|' -e 's|@dotool@|${pkgs.dotool}|' ${./record.sh} > $out
+      chmod +x $out
+    '';
+    whisper = pkgs.whisper-cpp.override {
+      cudaSupport = true;
     };
-
+  in {
     dconf.settings."org/gnome/shell" = {
       enabled-extensions = ["speechtotext@local"];
+    };
+
+    home.file = {
+      ".local/bin/stt-record.sh" = {
+        executable = true;
+        source = script;
+      };
+
+      "${extensionDirectory}/extension.js".source = ./extension.js;
+      "${extensionDirectory}/metadata.json".source = ./metadata.json;
+      "${extensionDirectory}/stylesheet.css".source = ./stylesheet.css;
+      "${extensionDirectory}/schemas" = {
+        recursive = true;
+        source = schemas;
+      };
     };
   };
 }
