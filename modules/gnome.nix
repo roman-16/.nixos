e274017da7bb4c728dae3d460a44b258957e3de7
Roman <roman@lerchster.dev>
2025-12-11 13:08:42 +0100

    2025-12-11 13:08:42

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -17,6 +17,7 @@
       gnomeExtensions.smile-complementary-extension
       smile
       gnomeExtensions.alphabetical-app-grid
+      gnomeExtensions.blurt
     ];
 
     environment.gnome.excludePackages = with pkgs; [
@@ -185,6 +186,7 @@
             gnomeExtensions.alphabetical-app-grid.extensionUuid
             gnomeExtensions.appindicator.extensionUuid
             gnomeExtensions.blur-my-shell.extensionUuid
+            gnomeExtensions.blurt.extensionUuid
             gnomeExtensions.burn-my-windows.extensionUuid
             gnomeExtensions.dash-to-panel.extensionUuid
             gnomeExtensions.ddterm.extensionUuid
