4ea6f90b7c9a2b11323e499204b8f352041151c9
Roman <roman@lerchster.dev>
2026-02-18 21:23:01 +0100

    2026-02-18 21:23:01

--- modules/fragments.nix
+++ modules/fragments.nix
@@ -3,11 +3,6 @@
     environment.systemPackages = with pkgs; [
       fragments
     ];
-
-    services.transmission = {
-      enable = true;
-      package = pkgs.transmission_4;
-    };
   };
 
   home = {};
