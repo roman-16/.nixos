84cf778998f0fbc1d91dd2349742d5ae7e49df22
Roman <roman@lerchster.dev>
2026-03-14 18:16:31 +0100

    2026-03-14 18:16:31

--- flake.nix
+++ flake.nix
@@ -12,6 +12,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     stylix.url = "github:danth/stylix";
   };
 
@@ -26,6 +30,7 @@
           ./hosts/roman-nixos/configuration.nix
           inputs.home-manager.nixosModules.default
           inputs.nix-flatpak.nixosModules.nix-flatpak
+          inputs.nix-index-database.nixosModules.default
           inputs.stylix.nixosModules.stylix
         ];
       };
