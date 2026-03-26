c2cf6aaa762fe083633e9e2b7a04c8da191ede7d
Roman <roman@lerchster.dev>
2026-03-27 00:07:48 +0100

    2026-03-27 00:07:48

--- hosts/roman-nixos/modules/pi/default.nix
+++ hosts/roman-nixos/modules/pi/default.nix
@@ -12,20 +12,30 @@
     extensionsDir = ./extensions;
     extensionFiles = builtins.attrNames (builtins.readDir extensionsDir);
 
-    skillsDir = ./skills;
-    skillDirs = builtins.attrNames (builtins.readDir skillsDir);
-
     extensionAttrs = builtins.listToAttrs (map (name: {
         name = ".pi/agent/extensions/${name}";
         value.source = extensionsDir + "/${name}";
       })
       extensionFiles);
 
-    # Skills are directories, symlink each one
-    skillAttrs = builtins.listToAttrs (map (name: {
-        name = ".pi/agent/skills/${name}";
-        value.source = skillsDir + "/${name}";
-      })
+    # Skills: symlink individual files so directories are real (writable for npm install)
+    skillsDir = ./skills;
+    skillDirs = builtins.attrNames (builtins.readDir skillsDir);
+
+    collectFiles = prefix: dir:
+      builtins.concatLists (builtins.attrValues (builtins.mapAttrs (name: type:
+        if type == "directory"
+        then collectFiles "${prefix}/${name}" (dir + "/${name}")
+        else [
+          {
+            name = "${prefix}/${name}";
+            value.source = dir + "/${name}";
+          }
+        ])
+      (builtins.readDir dir)));
+
+    skillAttrs = builtins.listToAttrs (builtins.concatMap
+      (name: collectFiles ".pi/agent/skills/${name}" (skillsDir + "/${name}"))
       skillDirs);
 
     settings = {
