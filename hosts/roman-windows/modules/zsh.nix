bb8bdcc9b6348bcec61fee3ab383b06d03d5a839
Roman <roman@lerchster.dev>
2026-05-22 16:39:26 +0200

    2026-05-22 16:39:26

--- hosts/roman-windows/modules/zsh.nix
+++ hosts/roman-windows/modules/zsh.nix
@@ -2,6 +2,6 @@
   nixos = {};
 
   home = {lib, ...}: {
-    programs.zsh.shellAliases.nx-update = lib.mkForce "nh os switch --update --hostname $(hostname); winget.exe upgrade --all --silent --include-unknown --accept-source-agreements --accept-package-agreements";
+    programs.zsh.shellAliases.nx-update = lib.mkForce "nh os switch --update --hostname $(hostname); winget.exe upgrade --all --include-unknown --accept-source-agreements --accept-package-agreements";
   };
 }
