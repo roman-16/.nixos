96b6b3c88825e38eebd10086cffab03226cb5b38
Roman <roman@lerchster.dev>
2026-06-14 13:58:10 +0200

    2026-06-14 13:58:10

--- hosts/roman-windows/modules/autostart.nix
+++ hosts/roman-windows/modules/autostart.nix
@@ -20,25 +20,5 @@
       # it. Kept disabled via the StartupApproved pin below.
       (run "Steam" "\"C:\\Program Files (x86)\\Steam\\steam.exe\" -silent")
     ];
-
-    home.activation = {
-      # Pin Steam's startup toggle to disabled (03..) so it can't silently
-      # re-enable itself across Steam updates.
-      steamKeepDisabled = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        reg="/mnt/c/Windows/System32/reg.exe"
-        [ -x "$reg" ] || exit 0
-        $DRY_RUN_CMD "$reg" add "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run" \
-          /v Steam /t REG_BINARY /d 030000000000000000000000 /f >/dev/null 2>&1 || true
-      '';
-
-      # 'CenterWindow' is the old name of the AutoHotkey autostart; its Run value
-      # is gone but the StartupApproved toggle lingers. Drop the orphan.
-      cleanupStaleAutostart = lib.hm.dag.entryAfter ["writeBoundary"] ''
-        reg="/mnt/c/Windows/System32/reg.exe"
-        [ -x "$reg" ] || exit 0
-        $DRY_RUN_CMD "$reg" delete "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\StartupApproved\\Run" \
-          /v CenterWindow /f >/dev/null 2>&1 || true
-      '';
-    };
   };
 }
