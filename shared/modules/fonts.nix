89f86eb60cd28c2da5c7543abe6e0745f6a7c3c2
Roman <roman@lerchster.dev>
2026-07-12 12:43:03 +0200

    2026-07-12 12:43:03

--- shared/modules/fonts.nix
+++ shared/modules/fonts.nix
@@ -1,43 +1,58 @@
 {
-  nixos = { pkgs, ... }: {
-    fonts.packages = with pkgs; [
-      fira-code
-      fira-mono
-      nerd-fonts.fira-code
-      nerd-fonts.fira-mono
-    ];
+  nixos =
+    { pkgs, ... }:
+    let
+      # Chromium/Electron UI (VS Code, Brave, ...) can't resolve Cantarell's
+      # variable-font-only build and falls back to a serif face, so ship the
+      # static instances instead. Upstream acknowledges this in cantarell-fonts
+      # NEWS (build statics "if you run into problems with the variable font");
+      # see also https://github.com/microsoft/vscode/issues/319988.
+      cantarell-static = pkgs.cantarell-fonts.overrideAttrs (old: {
+        mesonFlags = (old.mesonFlags or [ ]) ++ [
+          "-Dbuildstatics=true"
+          "-Dbuildvf=false"
+        ];
+      });
+    in
+    {
+      fonts.packages = with pkgs; [
+        fira-code
+        fira-mono
+        nerd-fonts.fira-code
+        nerd-fonts.fira-mono
+      ];
 
-    stylix = {
-      fonts = {
-        serif = {
-          package = pkgs.cantarell-fonts;
-          name = "Cantarell";
-        };
+      stylix = {
+        fonts = {
+          serif = {
+            package = cantarell-static;
+            name = "Cantarell";
+          };
 
-        sansSerif = {
-          package = pkgs.cantarell-fonts;
-          name = "Cantarell";
-        };
+          sansSerif = {
+            package = cantarell-static;
+            name = "Cantarell";
+          };
 
-        monospace = {
-          package = pkgs.fira-mono;
-          name = "FiraMono Nerd Font";
-        };
+          monospace = {
+            package = pkgs.fira-mono;
+            name = "FiraMono Nerd Font";
+          };
 
-        emoji = {
-          package = pkgs.noto-fonts-color-emoji;
-          name = "Noto Color Emoji";
-        };
+          emoji = {
+            package = pkgs.noto-fonts-color-emoji;
+            name = "Noto Color Emoji";
+          };
 
-        sizes = {
-          applications = 11;
-          desktop = 9;
-          popups = 9;
-          terminal = 11;
+          sizes = {
+            applications = 11;
+            desktop = 9;
+            popups = 9;
+            terminal = 11;
+          };
         };
       };
     };
-  };
 
   home = { };
 }
