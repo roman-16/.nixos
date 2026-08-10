5835e58b13de76d6410fe47a6bbd19a417884377
Roman <roman@lerchster.dev>
2026-08-10 10:29:44 +0200

    2026-08-10 10:29:44

--- hosts/roman-nixos/modules/system.nix
+++ hosts/roman-nixos/modules/system.nix
@@ -5,10 +5,7 @@
 
       loader = {
         efi.canTouchEfiVariables = true;
-        systemd-boot = {
-          configurationLimit = 8;
-          enable = true;
-        };
+        systemd-boot.enable = true;
       };
     };
 
@@ -33,17 +30,6 @@
           variant = "nodeadkeys";
         };
       };
-
-      udev.extraRules = ''
-        # HW.1, Nano
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c|2b7c|3b7c|4b7c", TAG+="uaccess", TAG+="udev-acl"
-
-        # Blue, NanoS, Aramis, HW.2, Nano X, NanoSP, Stax, Ledger Test,
-        SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", TAG+="uaccess", TAG+="udev-acl"
-
-        # Same, but with hidraw-based library (instead of libusb)
-        KERNEL=="hidraw*", ATTRS{idVendor}=="2c97", MODE="0666"
-      '';
     };
 
     users.users.roman = {
