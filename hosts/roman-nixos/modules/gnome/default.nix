024c02bfc3b8a72c97e39306bcd87827fd1c0d32
Roman <roman@lerchster.dev>
2026-08-16 21:29:17 +0200

    2026-08-16 21:29:17

--- hosts/roman-nixos/modules/gnome/default.nix
+++ hosts/roman-nixos/modules/gnome/default.nix
@@ -5,7 +5,7 @@
       murmur =
         let
           uuid = "murmur@roman-16.github.io";
-          version = "1.2.2";
+          version = "1.3.0";
         in
         # Murmur is written in TypeScript, so take the built extension from the
         # release asset rather than compiling the checkout.
@@ -14,7 +14,7 @@
           inherit version;
           src = pkgs.fetchurl {
             url = "https://github.com/roman-16/murmur/releases/download/v${version}/${uuid}.shell-extension.zip";
-            hash = "sha256-TNZw5hZmJXJkV61JXLHwJgzxq8ftVX5IH2BVFg0irQ0=";
+            hash = "sha256-Vw6iYFRZ2IUPNrIkrvRl4ipY3iNzn+wzNkIRwW5spb8=";
           };
           nativeBuildInputs = [
             pkgs.glib
